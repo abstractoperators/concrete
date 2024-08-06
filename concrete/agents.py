de2cefc8728a787bfad0e2c905f5c33e0d320aa4
Michael Deng <michael@abstractoperators.ai>
2024-08-06 12:18:42 -0500

    Update prompting in Agents (#17)

--- concrete/agents.py
+++ concrete/agents.py
@@ -30,7 +30,12 @@ class Agent:
         )
         self.assistant = self.clients["openai"].create_assistant(prompt=instructions)  # type: ignore
 
-    def _qna(self, content: str, thread: Thread | None = None, instructions: str | None = None):
+    def _qna(
+        self,
+        content: str,
+        thread: Thread | None = None,
+        instructions: str | None = None,
+    ):
         """
         "Question and Answer", given a query, return an answer.
 
@@ -85,28 +90,33 @@ class Developer(Agent):
 
         Can return `no question`.
         """
-        return f"""Context:
+        return dedent(
+            f"""
+            *Context:*
             {context}
 
-            If necessary, ask one essential question for continued implementation. If unnecessary, respond 'No Question'.
+            If necessary, ask one essential question for continued implementation.
+            If no question is necessary, respond 'No Question'.
 
-            Example:
-            Context:
-            1. Imported the Flask module from the flask package
-            Current Component: Create a Flask application instance
-            Clarification: None
+            **Example:**
+                Context:
+                1. Imported the Flask module from the flask package
+                Current Component: Create a Flask application instance
+                Clarification: None
 
-            No Question
+                No Question
 
 
-            Example:
-            Context:
-            1. The code imported the Flask module from the flask package
-            2. The code created a Flask application named "app"
-            3. Created a route for the root URL ('/')
-            Current Component: Create a function that will be called when the root URL is accessed. This function should return HTML with a temporary Title, Author, and Body Paragraph
+            **Example:**
+                Context:
+                1. The code imported the Flask module from the flask package
+                2. The code created a Flask application named "app"
+                3. Created a route for the root URL ('/')
+                Current Component: Create a function that will be called when the root URL is accessed.\
+                This function should return HTML with a temporary Title, Author, and Body Paragraph
 
-            What should the function be called?"""  # noqa: E501
+                What should the function be called?"""
+        )
 
     @Agent.qna
     def implement_component(self, context: str, dedent=True) -> str:
@@ -115,32 +125,56 @@ class Developer(Agent):
         Returns the code for the component
         """
         return f"""
-            Context:
+            Provide code for the current component based on existing component implementations in Context.\
+            Produced code blocks should be preceded by the file where it should be placed.
+            Use placeholders referencing code/functions already provided in the context. Never provide unspecified code.
+
+            *Context:*
             {context}
 
-            Based on the context, provide code for the current component. Name the file appropriately. Use placeholders referencing code already provided in the context. Never provide unspecified code.
 
-            Example:
-            Context:
-            1. Imported the Flask module from the flask package
-            Current Component: Create a flask application instance named 'app'
-            Clarification: None
+            **Example:**
+                Context:
+                1. Imported the Flask module from the flask package
+                Current Component: Create a flask application instance named 'app'
+                Clarification: None
 
-            app = Flask(app)
+                Output:
+                app.py
+                ```python
+                app = Flask(app)
+                ```
 
+            **Example:**
+                Context:
+                1. The code imported the Flask module from the flask package
+                2. The code created a Flask application named "app"
+                3. Created a route for the root URL ('/')
+                Current Component: Create a function that will be called when the root URL is accessed.
+                Question: What should the function do?
+                Clarification: The function should do nothing. It should be a placeholder for future functionality.
 
-            Example:
-            Context:
-            1. The code imported the Flask module from the flask package
-            2. The code created a Flask application named "app"
-            3. Created a route for the root URL ('/')
-            Current Component: Create a function that will be called when the root URL is accessed.
-            Clarification: The function should be called index
+                Output:
+                app.py
+                ```python
+                def index():
+                    pass
+                ```
 
-            def index():
-                return '''
-                <html></html>'''
-        """  # noqa: E501
+            **Example:**
+                Context:
+                    1. The code imported the Flask module from the flask package
+                    2. The code created a Flask application named "app"
+                    3. Created a route for the root URL ('/')
+                    4. Created a placeholder function named 'index' that does nothing
+                    Current Component: Create an html file that will be rendered when the root URL is accessed
+
+                    Output:
+                    templates/index.html
+                    ```html
+                    <!DOCTYPE html>
+                    ```html
+        """
 
     @Agent.qna
     def integrate_components(
@@ -155,19 +189,22 @@ class Developer(Agent):
         """
         prev_components = []
         for desc, code in zip(planned_components, implementations):
-            prev_components.append(f"\n\t Component description: {desc}" f"\n\t Code: {code}")
-        out_str = (
-            "\nTask: Combine all these implementations into a single, coherent final application\n"
-            f"""
-            Original Webpage Idea:
+            prev_components.append(f"\n\t****Component description****: \n{desc}\n\t****Code:**** \n{code}")
+
+        out_str = """\
+            *Task: Implement the original webpage creation task*
+            ```webpage_details
             {webpage_idea}
+            ```end_webpage_details
 
-            Component implementations: {"".join(prev_components)}
-
-            Ensure that:
-            1. All necessary imports are at the top of each file
+            **Important Details:**
+            1. All necessary imports and libraries are at the top of each file
             2. Each code block is preceded by a file path where it should be placed
-            Example:
+            3. Code is organized logically
+            4. Resolve duplicate and conflicting code with discretion
+            5. Only code and file paths are returned
+
+            **Example Output:**
             app.py
             ```python
             def foo():
@@ -178,10 +215,17 @@ class Developer(Agent):
             ```html
             <!DOCTYPE html>
             ```
-            3. Code is organized logically
-            4. There are no duplicate or conflicting code
-            5. Only code or file paths are returned
-            """
+            
+            static/style.css
+            ```css
+                /* Foo */
+            ```
+            
+            **Integrate the following components to implement the webpage**
+                ***Components:***
+                {{"".join(prev_components)}}
+            """.format(
+            webpage_idea=webpage_idea
         )
         CLIClient.emit("Integrate components:\n" + out_str)
         return out_str
@@ -189,18 +233,16 @@ class Developer(Agent):
     @Agent.qna
     def implement_html_element(self, prompt: str) -> str:
         out_str = f"""
-        Write an html element with the following description:
+        Generate an html element with the following description:\n
         {prompt}
 
         Example 1.
         Description: Create a header that says `Title`
-
-        <h1>Title</h1>
+        Output: <h1>Title</h1>
 
         Example 2.
-        Description: Create an input form that dispatches a POST request on submit
-
-        <form method="POST" action="/">
+        Description: Create an input form with a submit button
+        Output:<form method="POST" action="/">
         <label for="textInput">Input:</label>
         <input type="text" id="textInput" name="textInput" required>
         <button type="submit">Submit</button>
@@ -217,24 +259,20 @@ class Executive(Agent):
 
     @Agent.qna
     def plan_components(self) -> str:
-        return """
-        List, in natural language, only the essential code components needed to fulfill the user's request.
- 
+        return """\
+        List the essential code components needed to fulfill the user's request. Each component should be atomic,\
+        such that a developer could implement it in isolation provided placeholders for preceding  components.
+        
         Your response must:
-        1. Include only core components.
-        2. Put each new component on a new line (not numbered, but conceptually sequential).
-        3. Focus on the conceptual steps of specific code elements or function calls
-        4. Be comprehensive, covering all necessary components
-        5. Use technical terms appropriate for the specific programming language and framework.
-        6. Naturally sequence components, so that later components are dependent on previous ones.
- 
-        Important:
+        1. Focus on the conceptual steps of specific code elements or function calls
+        2. Be comprehensive, accurate, and complete; cover all necessary components
+        3. Use technical terms appropriate for the specific programming language and framework.
+        4. Sequence components logically, with later components dependent on previous ones
+        5. Put each  component on a new line without numbering
+
+        Assumptions:
         - Assume all dependencies are already installed but not imported.
-        - Do not include dependency installations.
-        - Focus solely on the code components needed to implement the functionality.
-        - NEVER provide code example
-        - ALWAYS ensure all necessary components are present
- 
+
         Example format:
         [Natural language specification of the specific code component or function call]
         [Natural language specification of the specific code component or function call]
@@ -266,35 +304,13 @@ class Executive(Agent):
 
         For each component summary:
         1. Describe its functionality using natural language
-        2. Include variable, function, and file names from components
+        2. Include file name, function name, and variable name in the description.
 
         Example:
-        1. imported the packages numpy and pandas as np and pd respectively. imported random
-        2. Instantiated a pandas dataframe named foo, with column names bar and baz
-        3. Populated foo with random ints
-        4. Printed average of bar and baz
-        """
-
-    @Agent.qna
-    def implement_html_element(self, prompt: str) -> str:
-        """
-        Accept a prompt to generate an html element
-        """
-        return f"""
-        Generate an html element with the following description:\n
-        {prompt}
-
-        Example 1.
-        Description: Create a header that says `Title`
-        Output: <h1>Title</h1>
-
-        Example 2.
-        Description: Create an input form with a submit button
-        Output:<form method="POST" action="/">
-        <label for="textInput">Input:</label>
-        <input type="text" id="textInput" name="textInput" required>
-        <button type="submit">Submit</button>
-    </form>
+        1. Imported the packages numpy as np, and pandas as pd in app.py
+        2. Instantiated a pandas dataframe named foo, with column names bar and baz in app.py
+        3. Populated foo with random ints in app.py
+        4. Printed average of bar and baz in the main function of app.py
         """
 
 
@@ -377,11 +393,11 @@ class AWSAgent:
         Assumes one file per filetype or language.
         """
         out_files: dict[str, str] = {}
-        file_string_lines = backend_code.strip().split('\n')
+        file_string_lines = backend_code.strip().split("\n")
 
         file_start_line, file_name = None, None
         for i, line in enumerate(file_string_lines):
-            if line.startswith('```'):
+            if line.startswith("```"):
                 if file_start_line is None:
                     # New file detected
                     file_name = file_string_lines[i - 1]
