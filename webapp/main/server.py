d3957ea9f3f99949a57fef5c383e419d58054546
Michael Deng <michaeldeng2003@gmail.com>
2024-08-08 12:56:25 -0500

    add favicon and abstract operators name to main website

--- webapp/main/server.py
+++ webapp/main/server.py
@@ -2,10 +2,12 @@ import os
 
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -15,7 +17,11 @@ async def root(request: Request):
 
 @app.get("/pitch-deck")
 async def get_pdf():
-    pdf_path = "abstractoperators_pitchdeck.pdf"
+    pdf_path = "static/abstractoperators_pitchdeck.pdf"
     if os.path.exists(pdf_path):
-        return FileResponse(pdf_path, filename="abstractoperators_pitchdeck.pdf", media_type="application/pdf")
+        return FileResponse(
+            pdf_path,
+            media_type="application/pdf",
+            headers={"Content-Disposition": "inline; filename=abstractoperators_pitchdeck.pdf"},
+        )
     return {"error": "PDF not found"}
