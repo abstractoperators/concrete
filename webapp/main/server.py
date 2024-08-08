22a15e4acac9e586dcba016ee51bfacef3b5ff53
Michael Deng <michaeldeng2003@gmail.com>
2024-08-08 12:39:13 -0500

    Add pdf download for pitch deck

--- webapp/main/server.py
+++ webapp/main/server.py
@@ -1,5 +1,7 @@
+import os
+
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -9,3 +11,11 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/pitch-deck")
+async def get_pdf():
+    pdf_path = "abstractoperators_pitchdeck.pdf"
+    if os.path.exists(pdf_path):
+        return FileResponse(pdf_path, filename="abstractoperators_pitchdeck.pdf", media_type="application/pdf")
+    return {"error": "PDF not found"}
