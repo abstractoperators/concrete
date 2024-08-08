ba315f54c96cd6eeac97e38a6c1ce79a02776737
Michael Deng <michaeldeng2003@gmail.com>
2024-08-08 09:28:47 -0500

    Move demo into webapp folder

--- webapp/demo/scripts/start.sh
+++ webapp/demo/scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd /app/demo_webapp
-ls
+cd /app/webapp/demo
 
 poetry run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
