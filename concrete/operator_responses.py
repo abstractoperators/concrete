e189ea10a50854060e39de7b23d91849d4ba31d8
Michael Deng <michaeldeng2003@gmail.com>
2024-08-14 10:18:21 -0500

    Remove individual __str__, and replace with Response string that dumps json

--- concrete/operator_responses.py
+++ concrete/operator_responses.py
@@ -13,42 +13,36 @@ Example:
     message_formatted: MyClass = message.parsed
 """
 
+from json import dumps
 from typing import List
 
 from pydantic import BaseModel
 
 
-class ProjectFile(BaseModel):
+class Response(BaseModel):
+    def __str__(self):
+        return dumps(self.model_dump(), indent=2)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class ProjectFile(Response):
     file_name: str
     file_contents: str
 
-    def __str__(self):
-        return f"File Name: {self.file_name}\n" f"File Contents: {self.file_contents}"
 
-
-class ProjectDirectory(BaseModel):
+class ProjectDirectory(Response):
     files: list[ProjectFile]
 
-    def __str__(self):
-        return "Final Files:\n" + "\n\n".join([str(file) for file in self.files])
 
-
-class TextResponse(BaseModel):
+class TextResponse(Response):
     text: str
 
-    def __str__(self):
-        return self.text
 
-
-class Summary(BaseModel):
+class Summary(Response):
     summary: List[str]
 
-    def __str__(self):
-        return 'Summary\n' + "\n".join(self.summary)
 
-
-class PlannedComponents(BaseModel):
+class PlannedComponents(Response):
     components: List[str]
-
-    def __str__(self):
-        return "\n".join([f"[Planned Component]: {component}" for component in self.components])
