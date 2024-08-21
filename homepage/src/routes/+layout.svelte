4831d9fc297fe544edf775398ba8f936821a5c00
Kent Gang <kent.y.gang@gmail.com>
2024-08-21 15:12:29 -0400

    Setup boilerplate svelte homepage

---
+++ homepage/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<div class="app">
+	<header><a href="https://github.com/abstractoperators/concrete">git</a> <a href="https://www.abop.ai/pitch-deck">deck</a></header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>created autonomously by abstract operators™️</p><p> copyright, 2024 © </p>
+	</footer>
+</div>
---
+++ homepage/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h3>abstract</h3>
+<h3>operators™️</h3>
