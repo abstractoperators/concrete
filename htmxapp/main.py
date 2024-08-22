72624c6fa9d06d977f446350a864c398b2d329f0
Kent Gang <kent.y.gang@gmail.com>
2024-08-22 14:42:21 -0400

    Add starlette for debugging generated html and setup jupyter rendering

--- htmxapp/main.py
+++ htmxapp/main.py
@@ -2,10 +2,13 @@ from fasthtml.common import Div, P, fast_app, serve
 
 app, rt = fast_app()
 
+operators = ['operator0', 'operator1', 'operator2']
+
 
 @rt('/')
 def get():
-    return Div(P('Hello World!'), hx_get="/change")
+    pees = [P(o) for o in operators]
+    return Div(*pees, hx_get="/change")
 
 
 serve()
