426f5abf8e72dac9fa05e4b38125e557e6467fa7
Kent Gang <kent.y.gang@gmail.com>
2024-09-02 13:00:40 -0400

    mv _operators -> operators.py

--- concrete/__init__.py
+++ concrete/__init__.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 
-from . import _operators, orchestrator
+from . import operators, orchestrator
 
 # Always runs even when importing submodules
 # https://stackoverflow.com/a/27144933
 load_dotenv()
-__all__ = ["_operators", "orchestrator"]
+__all__ = ["operators", "orchestrator"]
