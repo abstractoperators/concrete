2734d5ed39d5c052503d4334a7de8ef0e839d08b
Michael Deng <michaeldeng2003@gmail.com>
2024-09-03 11:28:33 -0500

    Add a sleep statement

--- webapp/demo/scripts/start.sh
+++ webapp/demo/scripts/start.sh
@@ -4,4 +4,5 @@ cd /app
 celery -A concrete worker --loglevel=INFO --detach
 
 cd /app/webapp/demo
+sleep 20
 poetry run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
