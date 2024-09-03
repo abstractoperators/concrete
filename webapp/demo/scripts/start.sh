5a64b8805ea92ff82ed2375c88cf2b985a7ba875
Michael Deng <michaeldeng2003@gmail.com>
2024-09-03 10:54:21 -0500

    Start celery in background of demo

--- webapp/demo/scripts/start.sh
+++ webapp/demo/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /app
-celery -A concrete worker --loglevel=INFO
+celery -A concrete worker --loglevel=INFO --detach
 
 cd /app/webapp/demo
 poetry run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
