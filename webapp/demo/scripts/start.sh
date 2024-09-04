b3b53edea2c22bb5784fa43ea9b6143fbe6cb928
Michael Deng <michaeldeng2003@gmail.com>
2024-09-03 20:03:44 -0500

    Add logging to detached celery worker

--- webapp/demo/scripts/start.sh
+++ webapp/demo/scripts/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
 cd /app
-celery -A concrete worker --loglevel=INFO --detach
+echo "Starting celery worker"
+celery -A concrete worker --loglevel=DEBUG --detach --logfile=/app/celery.log
+
+# TODO supervisord?
+# Start a background process to tail the Celery log
+tail -f /app/celery.log &
 
 cd /app/webapp/demo
-sleep 20
+echo "Starting gunicorn server"
 poetry run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
