a0bb4a5d5287f9bdfba20dc080768958c07e451d
Michael Deng <michaeldeng2003@gmail.com>
2024-09-05 10:28:30 -0500

    Added daemon dockerfile; Added smarter build time env loading

--- webapp/daemons/server.py
+++ webapp/daemons/server.py
@@ -1,6 +1,7 @@
 import hashlib
 import hmac
 import json
+import os
 from http.client import HTTPException
 
 from fastapi import FastAPI, Request
@@ -23,10 +24,9 @@ async def github_webhook(request: Request):
     """Receive GitHub webhook events."""
     raw_payload = await request.body()
     signature = request.headers.get("x-hub-signature-256")
-    secret_token = "foobarbaz"  # Defined in the GitHub app settings # nosec
 
     try:
-        verify_signature(raw_payload, secret_token, signature)
+        verify_signature(raw_payload, signature)
     except HTTPException as e:
         return {"error": str(e.detail)}, e.status_code
 
@@ -35,7 +35,7 @@ async def github_webhook(request: Request):
     return {"message": f"Received {payload['action']} event"}
 
 
-def verify_signature(payload_body, secret_token, signature_header):
+def verify_signature(payload_body, signature_header):
     """Verify that the payload was sent from GitHub by validating SHA256.
 
     Raise and return 403 if not authorized.
@@ -45,9 +45,15 @@ def verify_signature(payload_body, secret_token, signature_header):
         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
         signature_header: header received from GitHub (x-hub-signature-256)
     """
+    GH_WEBHOOK_SECRET = os.getenv("GH_WEBHOOK_SECRET")
+    if not GH_WEBHOOK_SECRET:
+        raise HTTPException(status_code=500, detail="GH_WEBHOOK_SECRET is not set")
+
     if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing")
+
+    hash_object = hmac.new(GH_WEBHOOK_SECRET.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+
     expected_signature = "sha256=" + hash_object.hexdigest()
     if not hmac.compare_digest(expected_signature, signature_header):
         raise HTTPException(status_code=403, detail="Request signatures didn't match!")
