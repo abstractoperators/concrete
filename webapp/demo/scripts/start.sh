23f02288df8f9c4236d0f0c1945bbdd10693de54
Michael Deng <michaeldeng2003@gmail.com>
2024-09-27 09:52:54 -0500

    Update demo build to also use multi-stage build

--- webapp/demo/scripts/start.sh
+++ webapp/demo/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /app/webapp/demo
+cd webapp/demo
 echo "Starting gunicorn server"
-poetry run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
+gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
