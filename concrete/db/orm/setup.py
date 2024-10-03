faca078619b1322c6801a0df56683514a821efb0
Michael Deng <michaeldeng2003@gmail.com>
2024-10-03 14:06:27 -0500

    Update sqlalchemy database url generation

--- concrete/db/orm/setup.py
+++ concrete/db/orm/setup.py
@@ -2,6 +2,7 @@ import os
 from contextlib import contextmanager
 
 from dotenv import load_dotenv
+from sqlalchemy import URL
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import Session as SQLModelSession
 from sqlmodel import create_engine
@@ -9,14 +10,22 @@ from sqlmodel import create_engine
 from concrete.clients import CLIClient
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///sql_app.db")
+SQLALCHEMY_DATABASE_URL = URL.create(
+    drivername=os.getenv("DB_DRIVER", "sqlite"),
+    username=os.getenv("DB_USERNAME", ""),
+    password=os.getenv("DB_PASSWORD", ""),
+    host=os.getenv("DB_HOST", ""),
+    port=int(os.getenv("DB_PORT", "")),
+    database=os.getenv("DB_DATABASE", ""),
+)
+
 CLIClient.emit(f'Connecting to database at {SQLALCHEMY_DATABASE_URL}')
 
-# https://github.com/fastapi/sqlmodel/issues/75
-if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+if SQLALCHEMY_DATABASE_URL.drivername == "sqlite":
     connect_args = {"check_same_thread": False}
 else:
     connect_args = {}
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
