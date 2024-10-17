0fa5d68b69151ffe8ea9055d7278f5398e1d042b
abjjabjj <dance@abstractoperators.ai>
2024-10-17 09:20:29 -0700

    create operator names for existing rows in db

--- migrations/versions/fc7723965373_title_columns_renamed_to_name.py
+++ migrations/versions/fc7723965373_title_columns_renamed_to_name.py
@@ -24,8 +24,32 @@ def upgrade() -> None:
     op.alter_column('message', 'type_name', new_column_name='type')
 
     op.add_column('operator', sa.Column('name', sqlmodel.sql.sqltypes.AutoString(length=64), nullable=False))
-    op.create_unique_constraint('no_duplicate_operators_per_orchestrator', 'operator', ['name', 'orchestrator_id'])
 
+    # Add unique names to existing operators
+    old_operators = sa.Table(
+        'operator',
+        sa.MetaData(),
+        sa.Column('id', sa.Uuid()),
+        sa.Column('name', sa.Unicode(length=64)),
+        sa.Column('title', sqlmodel.sql.sqltypes.AutoString(length=32)),
+    )
+    # Use Alchemy's connection and transaction to noodle over the data.
+    connection = op.get_bind()
+    # Select all existing names that need migrating.
+    results = connection.execute(
+        sa.select(
+            old_operators.c.id,
+            old_operators.c.title,
+        )
+    ).fetchall()
+    # Iterate over all selected data tuples.
+    for i, (id, title) in enumerate(results):
+        # Split the existing name into first and last.
+        new_name = f"{title}{i}"
+        # Update the new columns.
+        connection.execute(old_operators.update().where(old_operators.c.id == id).values(name=new_name))
+
+    op.create_unique_constraint('no_duplicate_operators_per_orchestrator', 'operator', ['name', 'orchestrator_id'])
     op.alter_column('orchestrator', 'type_name', new_column_name='type')
     op.alter_column('orchestrator', 'title', new_column_name='name')
     op.create_unique_constraint('no_duplicate_names_per_user', 'orchestrator', ['name', 'user_id'])
