6e91398e53b6a3cf718ffe5865eabdeb59462b5c
abjjabjj <dance@abstractoperators.ai>
2024-11-13 15:50:01 -0800

    testless, first iteration of DAGProject API

--- src/concrete-core/concrete/__init__.py
+++ src/concrete-core/concrete/__init__.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 
-from . import operators, orchestrators
+from . import abstract, models, operators, orchestrators
 
 # Always runs even when importing submodules
 # https://stackoverflow.com/a/27144933
 load_dotenv(override=True)
-__all__ = ["operators", "orchestrators"]
+__all__ = ["abstract", "models", "operators", "orchestrators"]
