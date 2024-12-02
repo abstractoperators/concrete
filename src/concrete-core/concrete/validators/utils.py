6a2a60205fe4367b332d16fa80bc152728d1b500
Michael Deng <michaeldeng2003@gmail.com>
2024-12-02 14:04:26 -0600

    Revise validator documentation; Fix bug in `is_valid_python`.

--- src/concrete-core/concrete/validators/utils.py
+++ src/concrete-core/concrete/validators/utils.py
@@ -3,8 +3,13 @@ from concrete.operators import Judge
 
 
 def is_valid_python(projectfile: ProjectFile) -> bool:
+    """
+    Utility function for checking if a projectfile is a valid python file.
+    Returns True if the file compiles, False otherwise
+    Raises a ValueError if the file extension is not .py
+    """
     try:
-        if projectfile.file_path.endswith(".py"):
+        if projectfile.file_name.endswith(".py"):
             compile(projectfile.file_contents, projectfile.file_name, "exec")
             return True
         raise ValueError("File is not a python file")
@@ -19,6 +24,5 @@ def qna_llm_as_a_judge(query: str, message: Message) -> Rating:
 
     judge = Judge()
     rating: Rating = judge.rate_simple(query, str(message), options={'response_format': Rating})  # type: ignore # noqa
-    # Do something with this rating ig
 
     return rating
