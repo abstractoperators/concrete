72f41a4eadffb1d9021fa2741b77c87271c5a234
Michael Deng <michaeldeng2003@gmail.com>
2024-12-05 11:32:51 -0600

    Unify start scripts. Update webapp/pyproject.toml to use pyjwt <- jwt.

--- webapp/api/scripts/start.sh
+++ webapp/api/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uv run gunicorn server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
+uv run gunicorn webapp.api.server:app --bind 0.0.0.0:80 --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 1 --timeout=2000
