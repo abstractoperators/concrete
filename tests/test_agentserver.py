3838b91553386a70a9998c504f5c7b2aa35eec54
Michael Deng <michaeldeng2003@gmail.com>
2025-01-09 10:05:18 -0600

    Create fixture for testing delete operators endpoint

--- tests/test_agentserver.py
+++ tests/test_agentserver.py
@@ -23,6 +23,10 @@ def mock_operator():
 
 
 def test_operator_chat_endpoint(mock_operator, setenv):
+    """
+    Tests /chat/{operator_id} endpoint to ensure that the chat method is called.
+    TODO: Finalize return value for endpoint, and test that as well.
+    """
     operator_id, mock_operator = mock_operator
 
     payload = "Mocked chat request"
@@ -36,3 +40,17 @@ def test_operator_chat_endpoint(mock_operator, setenv):
     mock_operator.chat.assert_called_once_with("Mocked chat request")
 
     agentserver.operators.pop(operator_id)
+
+
+def test_operator_delete_endpoint(mock_operator, setenv):
+    """
+    Tests /delete/{operator_id} endpoint to ensure that the operator is deleted.
+    TODO: Check db to make sure operator is deleted post stateful PR.
+    """
+    operator_id, mock_operator = mock_operator
+
+    response = test_client.delete(f"/operators/{operator_id}")
+
+    assert response.status_code == 200
+
+    assert operator_id not in agentserver.operators
