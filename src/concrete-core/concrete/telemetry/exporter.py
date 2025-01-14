5a00a07cbea871e2f8113f92dacc959a365e2705
Michael Deng <michaeldeng2003@gmail.com>
2025-01-14 09:57:43 -0600

    Write LogExporter for otel

--- src/concrete-core/concrete/telemetry/exporter.py
+++ src/concrete-core/concrete/telemetry/exporter.py
@@ -1,8 +1,14 @@
+import json
+import logging
 import typing
 from os import linesep
 
 from opentelemetry.sdk.trace import ReadableSpan
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SpanExportResult
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SpanExporter,
+    SpanExportResult,
+)
 
 
 class FileSpanExporter(ConsoleSpanExporter):
@@ -24,3 +30,26 @@ class FileSpanExporter(ConsoleSpanExporter):
         with open(self.filepath, 'a+', encoding='utf-8') as file:
             self.out = file
             return super().export(spans)
+
+
+class LogExporter(SpanExporter):
+    def __init__(
+        self,
+        logger: logging.Logger,
+    ):
+        self.logger = logger
+        self.logger.setLevel(logging.INFO)
+        if not self.logger.handlers:
+            raise ValueError("Logger must have at least one handler.")
+
+    def export(self, spans: typing.Sequence[ReadableSpan]) -> SpanExportResult:
+        for span in spans:
+            span_data = {
+                "name": span.name,
+                "start_time": span.start_time.isoformat(),
+                "end_time": span.end_time.isoformat(),
+                "attributes": span.attributes,
+                "status": span.status.name,
+            }
+            self.logger.info(f"Otel Span Logged: {json.dumps(span_data)}")
+        return SpanExportResult.SUCCESS
