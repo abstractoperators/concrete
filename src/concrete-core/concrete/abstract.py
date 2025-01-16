30411f9eae915c4f1fc11d325df898ccdf134772
Michael Deng <michaeldeng2003@gmail.com>
2025-01-16 13:55:37 -0600

    Update tool invocations to not hang forever if the tool returns nothing

--- src/concrete-core/concrete/abstract.py
+++ src/concrete-core/concrete/abstract.py
@@ -159,6 +159,7 @@ class AbstractOperator(metaclass=AbstractOperatorMetaclass):
             # It also makes it difficult to do a manual invocation of the tool.
             # However, it aligns with goals of wanting Operators to be able to use tools
             retry_ct = 0  # try thrice
+            # TODO: Async and give up after a certain amount of time
             while retry_ct < 3:
                 try:
                     if issubclass(type(answer), Tool) and answer.tool_name and answer.tool_method:
@@ -167,16 +168,22 @@ class AbstractOperator(metaclass=AbstractOperatorMetaclass):
                             tool_postface = 'Use the following results to answer the query.'
                             tool_postface += (
                                 f'The tool {answer.tool_name}.{answer.tool_method}'
-                                ' with arguments {answer.tool_parameters} yields:\n'
+                                f' with arguments {answer.tool_parameters} yields:\n'
                             )
                             tool_postface += str(resp)
-
-                            query = question_producer(*args, **kwargs) + tool_postface
-                            answer = self._qna(
-                                query,
-                                response_format=response_format,
-                                instructions=instructions,
+                        else:
+                            tool_postface = "Use the following results to answer the query."
+                            tool_postface += (
+                                f'The tool {answer.tool_name}.{answer.tool_method}'
+                                f' with arguments {answer.tool_parameters} did not return a result.'
                             )
+
+                        query = question_producer(*args, **kwargs) + tool_postface
+                        answer = self._qna(
+                            query,
+                            response_format=response_format,
+                            instructions=instructions,
+                        )
                     break
                 except AttributeError:
                     retry_ct += 1
